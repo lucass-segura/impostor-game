2735382ddc890440d6db81bcc3bdef3a52743c78
Ante Brähler <ahcwnh@gmail.com>
2025-01-28 22:13:23 +0000

    Update background color for join game text

--- src/components/MultiplayerSetup.tsx
+++ src/components/MultiplayerSetup.tsx
@@ -82,7 +82,7 @@ export const MultiplayerSetup = () => {
                 <span className="w-full border-t border-white/10" />
               </div>
               <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
+                <span className="bg-secondary px-2 text-muted-foreground">
                   Or join existing game
                 </span>
               </div>
