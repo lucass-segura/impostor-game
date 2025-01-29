531fd898b43c93a251cea983417f021f51544399
Ante Brähler <ahcwnh@gmail.com>
2025-01-29 15:37:42 +0100

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
